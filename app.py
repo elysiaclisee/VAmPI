4aff217f98b695ef0d6b99f0e717c1f2d303f53f
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-16 01:44:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,26 @@ import os
  DO NOTE: some functionalities will still be vulnerable even if the value is set to 0
           as it is a matter of bad practice. Such an example is the debug endpoint.
 '''
-
 app = vuln_app.app
-
-@app.after_request
-def add_security_headers(response):
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    response.headers['Server'] = 'Web-Server'  
-    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Cross-Origin-Resource-Policy'] = 'same-origin' 
-    return response
 
 @app.errorhandler(404)
 def not_found_error(error):
     response = jsonify({"error": "Not Found"})
     response.status_code = 404
     return add_security_headers(response)
-
-if __name__ == '__main__':
-    vuln_app.run(host='0.0.0.0', port=5000, debug=True)
+ 
+@app.after_request
+def add_security_headers(response):
+    #error 10021: X-Content-Type-Options Header Missing
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    #error 10036: Server Leaks Version Information (Ghi đè Header Server)
+    response.headers['Server'] = 'Web-Server'  
+    #error 10049: Storable and Cacheable Content (Tắt cache cho API)
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    #error 90004: Cross-Origin-Resource-Policy Header Missing
+    response.headers['Cross-Origin-Resource-Policy'] = 'same-origin' 
+    return response
 
 if __name__ == '__main__':
     vuln_app.run(host='0.0.0.0', port=5000, debug=True)
