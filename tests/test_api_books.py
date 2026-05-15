bfd7aa66652a61b1bbde5afdf5b9dd3d360763e6
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-16 00:12:01 +0700

    Update test_api_books.py

--- tests/test_api_books.py
+++ tests/test_api_books.py
@@ -12,12 +12,6 @@ def test_add_ok(client, auth_token):
     res = client.post('/books/v1', json=payload, headers=hd)
     assert res.status_code == 200
 
-def test_get_bola(client, auth_token):
-    config.vuln = True
-    hd = {'Authorization': f'Bearer {auth_token}'}
-    res = client.get('/books/v1/bookTitle1', headers=hd)
-    assert res.status_code == 200
-
 def test_get_secure(client, auth_token):
     config.vuln = False
     hd = {'Authorization': f'Bearer {auth_token}'}
