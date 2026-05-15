2ddc525d10d55356b67b5328a82b1a37e005146a
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-15 23:47:20 +0700

    Update test_api_users.py

--- tests/test_api_users.py
+++ tests/test_api_users.py
@@ -1,6 +1,5 @@
 import config
 import pytest
-import app as vampi_app
 from api_views.users import JSON_MIME
 
 def test_duplicate_user(client):
@@ -10,27 +9,25 @@ def test_duplicate_user(client):
     assert b"User already exists" in response.data
 
 def test_wrong_pwd(client):
-    vampi_app.vuln = True #cover line 102
-    payload = {"username": "admin", "password": "wrongpassword"}
+    config.vuln = True 
+    payload = {"username": "admin", "password": "wrong"}
     response = client.post('/users/v1/login', json=payload)
     assert b"Password is not correct" in response.data
 
 def test_wrong_pwd_secure(client):
     config.vuln = False
-    payload = {"username": "admin", "password": "wrongpassword"}
+    payload = {"username": "admin", "password": "wrong"}
     res = client.post('/users/v1/login', json=payload)
     assert b"Username or Password Incorrect" in res.data
 
 def test_get_me_profile(client, auth_token):
     headers = {'Authorization': f'Bearer {auth_token}'}
-    response = client.get('/users/v1/_me', headers=headers)
+    response = client.get('/me', headers=headers)
     assert response.status_code == 200
-    assert response.get_json()['data']['username'] == 'admin'
 
 def test_update_pwd_bola(client, auth_token):
-    vampi_app.vuln = True # line 197
+    config.vuln = True 
     headers = {'Authorization': f'Bearer {auth_token}'}
-    #attempt to change the pwd of name1 through username parameter on URL
     payload = {"password": "newpassword123"}
-    response = client.put('/users/v1/password/name1', json=payload, headers=headers)
+    response = client.put('/users/v1/name1/password', json=payload, headers=headers)
     assert response.status_code == 204
