60fdaeb444913216cc811cb45085923cbf4af95e
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-15 23:52:59 +0700

    Update test_api_users.py

--- tests/test_api_users.py
+++ tests/test_api_users.py
@@ -2,32 +2,32 @@ import config
 import pytest
 from api_views.users import JSON_MIME
 
-def test_duplicate_user(client):
-    payload = {"username": "admin", "password": "any", "email": "any@mail.com"}
-    response = client.post('/users/v1/register', json=payload)
-    assert response.status_code == 200
-    assert b"User already exists" in response.data
+def test_reg_dup(client):
+    payload = {"username": "admin", "password": "any", "email": "a@m.com"}
+    res = client.post('/users/v1/register', json=payload)
+    assert res.status_code == 200
+    assert b"User already exists" in res.data
 
-def test_wrong_pwd(client):
-    config.vuln = True 
+def test_login_vuln(client):
+    config.vuln = True
     payload = {"username": "admin", "password": "wrong"}
-    response = client.post('/users/v1/login', json=payload)
-    assert b"Password is not correct" in response.data
+    res = client.post('/users/v1/login', json=payload)
+    assert b"Password is not correct" in res.data
 
-def test_wrong_pwd_secure(client):
+def test_login_safe(client):
     config.vuln = False
     payload = {"username": "admin", "password": "wrong"}
     res = client.post('/users/v1/login', json=payload)
     assert b"Username or Password Incorrect" in res.data
 
-def test_get_me_profile(client, auth_token):
-    headers = {'Authorization': f'Bearer {auth_token}'}
-    response = client.get('/me', headers=headers)
-    assert response.status_code == 200
+def test_profile(client, auth_token):
+    hd = {'Authorization': f'Bearer {auth_token}'}
+    res = client.get('/me', headers=hd)
+    assert res.status_code == 200
 
-def test_update_pwd_bola(client, auth_token):
-    config.vuln = True 
-    headers = {'Authorization': f'Bearer {auth_token}'}
-    payload = {"password": "newpassword123"}
-    response = client.put('/users/v1/name1/password', json=payload, headers=headers)
-    assert response.status_code == 204
+def test_pwd_bola(client, auth_token):
+    config.vuln = True
+    hd = {'Authorization': f'Bearer {auth_token}'}
+    payload = {"password": "new_password"}
+    res = client.put('/users/v1/name1/password', json=payload, headers=hd)
+    assert res.status_code == 204
