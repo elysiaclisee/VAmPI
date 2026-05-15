74f3b62896b5e660e624ee8bd5fe5d3c2c55d555
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-15 23:38:39 +0700

    Update users.py

--- api_views/users.py
+++ api_views/users.py
@@ -165,7 +165,7 @@ def update_email(username):
                 return Response(error_message_helper("Please Provide a valid email address."), 400,
                                 mimetype=JSON_MIME)
         else:
-            regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+            regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
             if (re.search(regex, request_data.get('email'))):
                 user.email = request_data.get('email')
                 db.session.commit()
