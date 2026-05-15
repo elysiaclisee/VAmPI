a6bc0ac08fe47924adc56c497628e3707c1ef5e4
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-15 23:46:53 +0700

    Update test_api_books.py

--- tests/test_api_books.py
+++ tests/test_api_books.py
@@ -1,6 +1,5 @@
 import pytest
-import app as vampi_app
-from api_views.books import JSON_MIME
+import config
 
 def test_get_all_books(client, auth_token):
     headers = {'Authorization': f'Bearer {auth_token}'}
@@ -10,41 +9,32 @@ def test_get_all_books(client, auth_token):
 
 def test_add_new_book_success(client, auth_token):
     headers = {'Authorization': f'Bearer {auth_token}'}
-    payload = {
-        "book_title": "DevSecOps Handbook",
-        "secret": "Security is a process, not a product."
-    }
-    response = client.post('/books/v1/add', json=payload, headers=headers)
+    payload = {"book_title": "New Title", "secret": "Secret Content"}
+    response = client.post('/books/v1/books', json=payload, headers=headers)
     assert response.status_code == 200
     assert b"Book has been added." in response.data
 
 def test_add_new_book_invalid(client, auth_token):
     headers = {'Authorization': f'Bearer {auth_token}'}
-    payload = {"book_title": "Invalid Book"}
-    response = client.post('/books/v1/add', json=payload, headers=headers)
+    payload = {"book_title": "Invalid"}
+    response = client.post('/books/v1/books', json=payload, headers=headers)
     assert response.status_code == 400
-    assert b"Please provide a proper JSON body." in response.data
+    assert b"proper JSON body" in response.data
 
 def test_add_new_book_unauthorized(client):
-    payload = {"book_title": "Ghost Book", "secret": "No token"}
-    response = client.post('/books/v1/add', json=payload)
+    payload = {"book_title": "Ghost", "secret": "None"}
+    response = client.post('/books/v1/books', json=payload)
     assert response.status_code == 401
 
 def test_get_by_title_bola(client, auth_token):
-    vampi_app.vuln = True
+    config.vuln = True 
     headers = {'Authorization': f'Bearer {auth_token}'}
-    response = client.get('/books/v1/bookTitle1', headers=headers)
+    response = client.get('/books/v1/book/bookTitle1', headers=headers)
     assert response.status_code == 200
-    assert 'secret' in response.get_json()
 
 def test_get_by_title_secure_access_denied(client, auth_token):
-    vampi_app.vuln = False
+    config.vuln = False 
     headers = {'Authorization': f'Bearer {auth_token}'}
-    response = client.get('/books/v1/bookTitle1', headers=headers)
+    response = client.get('/books/v1/book/bookTitle1', headers=headers)
     assert response.status_code == 404
     assert b"Book not found!" in response.data
-
-def test_get_by_title_not_found(client, auth_token):
-    headers = {'Authorization': f'Bearer {auth_token}'}
-    response = client.get('/books/v1/NonExistingBook', headers=headers)
-    assert response.status_code == 404
