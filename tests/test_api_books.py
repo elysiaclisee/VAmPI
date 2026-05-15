ee432d31e2072e61755fab5657a45a7a0a5970c9
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-16 01:39:46 +0700

    Update test_api_books.py

--- tests/test_api_books.py
+++ tests/test_api_books.py
@@ -8,20 +8,12 @@ def test_get_all(client, auth_token):
 
 def test_add_ok(client, auth_token):
     hd = {'Authorization': f'Bearer {auth_token}'}
-    payload = {"book_title": "Unique_Title_Final", "secret": "No_Comment"}
+    payload = {"book_title": "Unique_DevSecOps", "secret": "Clean_Code"}
     res = client.post('/books/v1', json=payload, headers=hd)
     assert res.status_code == 200
 
-def test_get_bola(client, auth_token):
-    config.vuln = True
-    hd = {'Authorization': f'Bearer {auth_token}'}
-    res = client.get('/books/v1/bookTitle1', headers=hd)
-    assert res.status_code == 404
-
-def test_get_secure(client):
+def test_get_secure(client, auth_token):
     config.vuln = False
-    login_res = client.post('/users/v1/login', json={"username": "admin", "password": "pass1"})
-    token = login_res.get_json()['auth_token']
-    hd = {'Authorization': f'Bearer {token}'}
+    hd = {'Authorization': f'Bearer {auth_token}'}
     res = client.get('/books/v1/bookTitle1', headers=hd)
     assert res.status_code == 404
