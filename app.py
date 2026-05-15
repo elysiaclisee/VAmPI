91e821a049d455ea89ddfe3fb8395fcdb4b77cd0
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-05-15 23:18:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from config import vuln_app
+from config import vuln_app, vuln, alive
 import os
 
 '''
@@ -6,12 +6,6 @@ import os
  DO NOTE: some functionalities will still be vulnerable even if the value is set to 0
           as it is a matter of bad practice. Such an example is the debug endpoint.
 '''
-vuln = int(os.getenv('vulnerable', 1))
-# vuln=1
-# token alive for how many seconds?
-alive = int(os.getenv('tokentimetolive', 60))
 
-
-# start the app with port 5000 and debug on!
 if __name__ == '__main__':
     vuln_app.run(host='0.0.0.0', port=5000, debug=True)
