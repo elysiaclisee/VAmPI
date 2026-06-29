4f0d3d1a0658d47c4d565da3fbc6eab91bd1abf6
Linh Nguyen <Linh.NK2416805@sis.hust.edu.vn>
2026-06-29 11:38:38 +0700

    remodeled

--- api_views/users.py
+++ api_views/users.py
@@ -2,7 +2,7 @@ import re
 import jsonschema
 import jwt
 
-from config import db, vuln
+from config import db, vuln_app
 #Import only needed names or import the module and then use its members.
 from api_views.json_schemas import register_user_schema, login_user_schema, update_email_schema
 from flask import jsonify, Response, request, json
@@ -21,20 +21,46 @@ def error_message_helper(msg):
 
 def get_all_users():
     resp = token_validator(request.headers.get('Authorization'))
+    
+    if isinstance(resp, str): 
+        return Response(error_message_helper(resp), 401, mimetype=JSON_MIME)
     if "error" in resp:
         return Response(error_message_helper(resp), 401, mimetype=JSON_MIME)
 
-    user = User.query.filter_by(username=resp['sub']).first()
-    if not user.admin:
-        return Response(error_message_helper("Unauthorized. Only admins can view the user directory."), 403, mimetype=JSON_MIME)
+    current_username = resp['sub']
+    user_record = User.query.filter_by(username=current_username).first()
 
-    return_value = jsonify({'users': User.get_all_users()})
-    return return_value
+    # RBAC
+    if not user_record or not user_record.admin:
+        return Response(error_message_helper("Unauthorized! Admin access required to view all users."), 403, mimetype=JSON_MIME)
 
+    list_of_users = []
+    users = User.query.all()
+    for user in users:
+        user_data = {}
+        user_data['username'] = user.username
+        user_data['email'] = user.email
+        user_data['admin'] = user.admin
+        list_of_users.append(user_data)
+
+    return Response(json.dumps({"status": "success", "users": list_of_users}), 200, mimetype=JSON_MIME)
 
 def debug():
-    return Response(error_message_helper("Resource not found"), 404, mimetype=JSON_MIME)
+    resp = token_validator(request.headers.get('Authorization'))
+    
+    if isinstance(resp, str): 
+        return Response(error_message_helper(resp), 401, mimetype=JSON_MIME)
+    if "error" in resp:
+        return Response(error_message_helper(resp), 401, mimetype=JSON_MIME)
 
+    current_username = resp['sub']
+    req_user = User.query.filter_by(username=current_username).first()
+    
+    if not req_user or not req_user.admin:
+        return Response(error_message_helper("Unauthorized! Top secret debug info is for Admins only."), 403, mimetype=JSON_MIME)
+
+    return_value = jsonify({'users': User.get_all_users_debug()})
+    return return_value
 
 def me():
     resp = token_validator(request.headers.get('Authorization'))
@@ -54,12 +80,22 @@ def me():
         
 
 def get_by_username(username):
+    resp = token_validator(request.headers.get('Authorization'))
+    if "error" in resp:
+        return Response(error_message_helper(resp), 401, mimetype=JSON_MIME)
+
+    current_user = resp['sub']
+    user_record = User.query.filter_by(username=current_user).first()
+    is_admin = user_record.admin if user_record else False
+
+    if current_user != username and not is_admin:
+        return Response(error_message_helper("Unauthorized! You cannot view other users' data."), 403, mimetype=JSON_MIME)
+
     if User.get_user(username):
         return Response(str(User.get_user(username)), 200, mimetype=JSON_MIME)
     else:
         return Response(error_message_helper("User not found"), 404, mimetype=JSON_MIME)
-
-
+    
 def register_user():
     request_data = request.get_json()
     user = User.query.filter_by(username=request_data.get('username')).first()
@@ -188,23 +224,17 @@ def update_password(username):
     
     if "error" in resp:
         return Response(error_message_helper(resp), 401, mimetype=JSON_MIME)
-
-    logged_in_username = resp['sub']
-    logged_in_user = User.query.filter_by(username=logged_in_username).first()
-    
-    if username != logged_in_username and not logged_in_user.admin:
-        return Response(error_message_helper("Unauthorized. You can only change your own password."), 403, mimetype=JSON_MIME)
         
     if request_data.get('password'):
-        target_user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=resp['sub']).first()
         
-        if target_user:
-            target_user.password = request_data.get('password')
+        if user:
+            user.password = request_data.get('password')
             db.session.commit()
             
             responseObject = {
                 'status': 'success',
-                'message': f'Password successfully updated for {username}.'
+                'Password': 'Updated.'
             }
             return Response(json.dumps(responseObject), 200, mimetype=JSON_MIME)
         else:
